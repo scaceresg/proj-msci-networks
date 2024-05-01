43d5c57b4e98001aece08326c7dbd991f47b60d3
CaptainJTT <102702516+CaptainJTT@users.noreply.github.com>
2024-05-01 18:37:07 +0100

    Update network.py

--- network.py
+++ network.py
@@ -12,3 +12,20 @@ class Network:
     def this_method(self):
 
         print('Malek is too slow')
+
+class Instance:
+    def __init__(self, n, k, seed):
+        self.n = n
+        self.k = k
+        np.random.seed = seed
+        self.weights = np.random.randint(1, 100, size=(n, n, k))
+
+        # 'Remove' loops:
+        for i in range(n):
+            self.weights[i, i, :] = 100000
+        self.weights[0, n-1] = 100000
+        self.weights[n-1, 0] = 100000
+
+n_nodes = 10
+n_scenario = 2
+instance = Instance(n_nodes, n_scenario, 1234)
